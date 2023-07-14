97b70a25e306581a96af04f9af2ae7e08cda1add
Ben LaRock <benlarock@gmail.com>
2023-07-14 15:58:32 -0400

    Linked list pop, append, and prepend methods

--- LinkedLists/ll_constructor.py
+++ LinkedLists/ll_constructor.py
@@ -9,16 +9,75 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+    
+    def __str__(self):
+        return (
+            f"Head node: {self.head}\n" +
+            f"Head value: {self.head.value}\n" +
+            f"Head next: {self.head.next}\n" +
+            f"Tail node: {self.tail}\n" +
+            f"Tail value: {self.tail.value}\n" +
+            f"Tail next: {self.tail.next}\n\n"
+            f"Number of nodes: {self.length}"
+        )
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
     def append(self, value):
-        pass
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        # return none when empty
+        if self.length == 0:
+            return None
+
+
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next # keep temp 1 step ahead of pre
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        # when already popped down to 1 item left
+        if (self.length == 0):
+            self.head = None
+            self.tail = None
+
+        return temp
 
     def prepend(self, value):
-        pass
+        new_node = Node(value)
 
-    def insert(self, value):
-        pass
+        if (self.length == 0):
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        
+        self.length += 1
+        return True
 
-linked_list = LinkedList(4)
+    # def insert(self, value):
+    #     pass
 
-print(linked_list.head.value)
+linked_list = LinkedList(2)
+linked_list.append(3)
+linked_list.prepend(1)
+
+linked_list.print_list()
